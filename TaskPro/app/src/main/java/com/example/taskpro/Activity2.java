2a3175bc7caf010269a0aff1d82b158c15d163b0
dimoDani <danidiazmoka@gmail.com>
2023-11-14 19:28:55 +0100

    Second Activity2 version

--- TaskPro/app/src/main/java/com/example/taskpro/Activity2.java
+++ TaskPro/app/src/main/java/com/example/taskpro/Activity2.java
@@ -2,16 +2,20 @@ package com.example.taskpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.database.sqlite.SQLiteDatabase;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 
 public class Activity2 extends AppCompatActivity {
+    private SQLiteDatabase db;
     private EditText plainTextTitle, plainTextDesc, date;
     private Button buttonAccept1;
     private ImageButton buttonReturn;
@@ -20,10 +24,23 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        db = openOrCreateDatabase("TaskPro", MODE_PRIVATE, null);
 
         plainTextTitle = findViewById(R.id.plainTextTitle1);
         plainTextDesc = findViewById(R.id.plainTextDesc1);
         date = findViewById(R.id.date1);
+
+        int itemId = getIntent().getIntExtra("id", -1);
+
+        if (itemId != -1) {
+            Cursor cursor = db.rawQuery("SELECT * FROM tasks WHERE _id = ?", new String[]{String.valueOf(itemId)});
+            if (cursor != null && cursor.moveToFirst()) {
+                plainTextTitle.setText(cursor.getString(1));
+                plainTextDesc.setText(cursor.getString(2));
+                date.setText(cursor.getString(3));
+                cursor.close();
+            }
+        }
 
         buttonAccept1 = findViewById(R.id.buttonAccept1);
         buttonReturn = findViewById(R.id.buttonReturn);
@@ -34,11 +51,16 @@ public class Activity2 extends AppCompatActivity {
                 String title = plainTextTitle.getText().toString();
                 String desc = plainTextDesc.getText().toString();
                 String taskDate = date.getText().toString();
-                Intent intent = new Intent(Activity2.this, Activity4.class);
-                intent.putExtra("title", title);
-                intent.putExtra("desc", desc);
-                intent.putExtra("taskDate", taskDate);
-                startActivity(intent);
+                if (title.isEmpty() || desc.isEmpty() || taskDate.isEmpty()) {
+                    Toast.makeText(Activity2.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(Activity2.this, Activity4.class);
+                    intent.putExtra("id", itemId);
+                    intent.putExtra("title", title);
+                    intent.putExtra("desc", desc);
+                    intent.putExtra("taskDate", taskDate);
+                    startActivity(intent);
+                }
             }
         });
 
