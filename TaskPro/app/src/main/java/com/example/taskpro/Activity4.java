4a7a3af62af32f422e97e35a04a1b9d72b23ae6f
salineroalex <alexsalineronegro@gmail.com>
2023-11-15 17:15:16 +0100

    Functional activity 4 and added app icon

--- TaskPro/app/src/main/java/com/example/taskpro/Activity4.java
+++ TaskPro/app/src/main/java/com/example/taskpro/Activity4.java
@@ -12,10 +12,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
-public class Activity4 extends AppCompatActivity implements OnClickListener{
+public class Activity4 extends AppCompatActivity implements OnClickListener {
 
     Intent intent_new = null;
 
@@ -39,6 +43,7 @@ public class Activity4 extends AppCompatActivity implements OnClickListener{
         date.setEnabled(false);
         buttonCalendar = (ImageButton) findViewById(R.id.buttonCalendar);
         buttonCalendar.setOnClickListener(this);
+        buttonCalendar.setVisibility(View.GONE);
         buttonCancelar = (Button) findViewById(R.id.buttonCancel1);
         buttonCancelar.setOnClickListener(this);
         buttonAceptar = (Button) findViewById(R.id.buttonAccept1);
@@ -48,25 +53,47 @@ public class Activity4 extends AppCompatActivity implements OnClickListener{
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()){
-        case R.id.buttonCalendar:
-            intent_new = new Intent(Intent.ACTION_EDIT);
-            intent_new.setType("vnd.android.cursor.item/event");
-            intent_new.putExtra(CalendarContract.Events.TITLE, textViewTitle.getText().toString());
-            intent_new.putExtra(CalendarContract.Events.DESCRIPTION, textViewDescription.getText().toString());
-            intent_new.putExtra(CalendarContract.Events.ALL_DAY, true);
-            intent_new.putExtra(CalendarContract.Events.DTSTART, date.getText().toString());
-            Intent chooser = intent_new.createChooser(intent_new, getString(R.string.a4_chooseApp));
-            startActivity(chooser);
-            break;
-        
-            case R.id.buttonCancel1:
-            // Code for verification when type: edit.
-            finish();
+        switch (v.getId()) {
+            case R.id.buttonCalendar:
+                String fechaTexto = date.getText().toString();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                Date fecha;
 
-        case R.id.buttonAccept1:
-            // Code for saving when type: edit.
-            finish();
+                try {
+                    fecha = dateFormat.parse(fechaTexto);
+                    long startMillis = fecha.getTime();
+                    long endMillis = startMillis + (24 * 60 * 60 * 1000);
+
+                    Intent intent_new = new Intent(Intent.ACTION_EDIT);
+                    intent_new.setType("vnd.android.cursor.item/event");
+                    intent_new.putExtra(CalendarContract.Events.DTSTART, startMillis);
+                    intent_new.putExtra(CalendarContract.Events.DTEND, endMillis);
+                    intent_new.putExtra(CalendarContract.Events.ALL_DAY, true);
+                    intent_new.putExtra(CalendarContract.Events.TITLE, textViewTitle.getText().toString());
+                    intent_new.putExtra(CalendarContract.Events.DESCRIPTION, textViewDescription.getText().toString());
+
+                    Intent chooser = intent_new.createChooser(intent_new, getString(R.string.a4_chooseApp));
+                    startActivity(chooser);
+                } catch (ParseException e) {
+                    Toast.makeText(Activity4.this, "Couldn't parse date", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.buttonCancel1:
+                finish();
+                break;
+
+            case R.id.buttonAccept1:
+                // Code for verification when type: edit.
+                buttonCalendar.setEnabled(true);
+                buttonCalendar.setVisibility(View.VISIBLE);
+                buttonCancelar.setEnabled(false);
+                buttonCancelar.setVisibility(View.GONE);
+                buttonAceptar.setEnabled(false);
+                //finish();
+                break;
+            default:
+
         }
     }
 }
